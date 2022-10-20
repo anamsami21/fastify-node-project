a975f66833840004a8276fa0de5a1611cf496f89
anamsami <anam.sami@thoughtworks.com>
2022-10-20 12:57:27 -0400

    change test for CI/CD change

--- tests/routes/root-route.test.js
+++ tests/routes/root-route.test.js
@@ -15,6 +15,6 @@ const res =  await  app.inject({
     url: '/',
 });
 expect(res.statusCode).toBe(200);
-expect(res.json()).toEqual({hello: 'world'});
+expect(res.json()).toEqual({ hello: 'world. I am done with CI/CD' });
 });
 })
